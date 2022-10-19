b20bd4e4a50279c742e66e53c390a78aee6a6f06
nathnaeltk <hellonathnael@gmail.com>
2022-10-19 13:50:48 +0300

    js modif

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 var buttonColours = ["orange", "green", "blue", "yellow"];
 
+// instruction and input pattern
 var gamePattern = [];
 var userClickedPattern = [];
 
 var started = false;
 var level = 0;
 
+// start game function
 $(document).keypress(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
@@ -15,6 +17,7 @@ $(document).keypress(function() {
   }
 });
 
+//click responses
 $(".btn").click(function() {
 
   var userChosenColour = $(this).attr("id");
@@ -26,6 +29,7 @@ $(".btn").click(function() {
   checkAnswer(userClickedPattern.length-1);
 });
 
+// array matching check loop
 function checkAnswer(currentLevel) {
 
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
@@ -48,6 +52,7 @@ function checkAnswer(currentLevel) {
 }
 
 
+// next level function, push another color to the array
 function nextSequence() {
   userClickedPattern = [];
   level++;
@@ -60,6 +65,7 @@ function nextSequence() {
   playSound(randomChosenColour);
 }
 
+// animation function
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
@@ -67,11 +73,13 @@ function animatePress(currentColor) {
   }, 100);
 }
 
+// play button sound function
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
+// reset function
 function startOver() {
   level = 0;
   gamePattern = [];
