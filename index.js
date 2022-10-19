d01d59d3a5f6a3b54a83fdb742c2c5346a3aad94
Nathnael Teketel <hellonathnael@gmail.com>
2022-10-19 13:58:33 +0300

    framing fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-var buttonColours = ["orange", "green", "blue", "yellow"];
+var buttonColors = ["orange", "green", "blue", "yellow"];
 
 // instruction and input pattern
 var gamePattern = [];
@@ -20,11 +20,11 @@ $(document).keypress(function() {
 //click responses
 $(".btn").click(function() {
 
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
 
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
 
   checkAnswer(userClickedPattern.length-1);
 });
@@ -41,7 +41,7 @@ function checkAnswer(currentLevel) {
     } else {
       playSound("wrong");
       $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart");
+      $("#level-title").text("GAME OVER! PRESS ANY KEY");
 
       setTimeout(function () {
         $("body").removeClass("game-over");
@@ -52,17 +52,18 @@ function checkAnswer(currentLevel) {
 }
 
 
+
 // next level function, push another color to the array
 function nextSequence() {
   userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
 
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColour);
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
 }
 
 // animation function
